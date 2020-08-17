b94daa5d0813aa5cee36c8571fe83e5d02101bf1
shalini0316 <shalini.jay1603@gmail.com>
2020-08-17 17:22:38 +0530

    Updates

---
+++ precache-manifest.a646e6fd64091ab447c1e5ea9e2c15a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae698b45280ed70135a9e86db1e583f9",
+    "url": "/react-appname/index.html"
+  },
+  {
+    "revision": "7a54f60ef7ef2ac1204e",
+    "url": "/react-appname/static/css/main.84d84195.chunk.css"
+  },
+  {
+    "revision": "e452cf72fd3f28aa8336",
+    "url": "/react-appname/static/js/2.0f578477.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-appname/static/js/2.0f578477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a54f60ef7ef2ac1204e",
+    "url": "/react-appname/static/js/main.57941d5e.chunk.js"
+  },
+  {
+    "revision": "f89a8ce2d9e79a14d5d3",
+    "url": "/react-appname/static/js/runtime-main.05c0573a.js"
+  }
+]);
